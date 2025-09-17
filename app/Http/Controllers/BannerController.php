0a233d97f1ec59c7ed01a1f51c89d24487194c2e
rancagp <ranca632@gmail.com>
2025-09-17 08:33:24 +0700

    size image

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -36,7 +36,7 @@ class BannerController extends Controller
         $validated = $request->validate([
             'title' => 'required|string|max:255',
             'description' => 'required|string',
-            'image' => 'required|image|mimes:jpeg,png,jpg|max:5120',
+            'image' => 'required|image|mimes:jpeg,png,jpg|max:20480',
             'order' => 'nullable|integer|min:1',
             'is_active' => 'required|boolean',
         ]);
